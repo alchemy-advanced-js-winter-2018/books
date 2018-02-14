163ee3c72c86cbac9a1ec41716c42500205ef349
Charlie Heiner <heinercj@gmail.com>
2018-02-14 15:30:54 -0800

    api request set up in book-api.js file

---
+++ src/services/book-api.js
@@ -0,0 +1,12 @@
+const API_KEY = 'AIzaSyAoODC168yeXM1wuYrPRWKDiYSzaRa4kwU';
+const URL = 'https://www.googleapis.com/books/v1/volumes?q=intitle:';
+
+
+//dog&key=AIzaSyAoODC168yeXM1wuYrPRWKDiYSzaRa4kwU
+
+export function getBooks(search){
+  const url = `${URL}${search}&key=${API_KEY}`;
+
+  return fetch(url)
+    .then(response => response.json());
+}
