fdcad624c4f1b6b2469cdcc91f1459b5cf207690
CharlyWelch <blwbiology@gmail.com>
2018-02-14 16:59:00 -0800

    include all base code for app

--- src/services/book-api.js
+++ src/services/book-api.js
@@ -1,9 +1,9 @@
-const URL = 'https://www.googleapis.com/books/v1/volumes?q=intitle:';
+const URL = 'https://www.googleapis.com/books/v1/volumes?q=subject:';
 const API_KEY = 'AIzaSyCnNP1g1nyTkcW6BjorD93gN-ueMhUWg0I';
 
-export function getBooks(search, page = 1, pageSize = 20) {
-    const url = `${URL}&q=${search}&page=${page}&pageSize=${pageSize}&key=${API_KEY}`;
+export function getBooks(search, page = 1, maxResults = 20) {
+  const url = `${URL}&q=${search}&page=${page}&maxResults=${maxResults}&orderBy=newest&key=${API_KEY}`;
 
-    return fetch(url)
+  return fetch(url)
     .then(response => response.json());
 }
